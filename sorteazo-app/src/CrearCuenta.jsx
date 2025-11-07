cf74de86f57689bd8959cc9eab8ba9ed32dec870
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-11-06 18:36:11 -0700

    Correcciones por nuevo logo

--- sorteazo-app/src/CrearCuenta.jsx
+++ sorteazo-app/src/CrearCuenta.jsx
@@ -18,10 +18,10 @@ export function CrearCuenta() {
                     <img
                         src={sorteazoLogo}
                         alt="Sorteazo logo"
-                        className="max-w-50"
+                        className="max-h-25"
                     />
                 </div>
-                <div className="flex flex-col align-center mt-10 gap-10 px-10 w-full h-full">
+                <div className="flex flex-col align-center gap-10 px-10 w-full h-full">
                     <div className="w-full max-w-lg mx-auto">
                         <div className="w-full">
                             <ToggleButtonText isCrearCuenta={true} />
