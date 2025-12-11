43ddfe570ea14522815d45034006d2c803ad16ed
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-12-11 14:04:37 -0600

    Campos de Registro y Login validados

--- sorteazo-web-api/src/users/dto/create-client.dto.ts
+++ sorteazo-web-api/src/users/dto/create-client.dto.ts
@@ -1,12 +1,16 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length, Matches, MaxLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class CreateClientDto extends CreateUserDto {
+
+    @IsNotEmpty({ message: 'La dirección es obligatoria.' })
     @IsString()
-    @IsNotEmpty()
+    @Length(5, 50, { message: 'El tamaño de la dirección es inválido.' })
     address: string;
 
+    @Matches(/^[0-9]{5}$/, { message: 'El código postal debe tener exactamente 5 dígitos.' })
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'El código postal es obligatorio.' })
+    //@Length(5, 5, { message: 'El código postal debe tener exactamente 5 dígitos.' })
     zipCode: string;
 }
--- sorteazo-web-api/src/users/dto/create-user.dto.ts
+++ sorteazo-web-api/src/users/dto/create-user.dto.ts
@@ -1,8 +1,9 @@
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, Length, MinLength, Matches } from 'class-validator';
 
 export class CreateUserDto {
 
     @IsString()
+    @Length(2, 30, { message: 'Por favor ingrese un nombre con un tamaño válido.' })
     @IsNotEmpty()
     name: string;
 
@@ -10,12 +11,12 @@ export class CreateUserDto {
     @IsNotEmpty()
     email: string;
 
-    @IsPhoneNumber('MX', { message: 'Please provide a valid Mexican phone number' })
+    //@IsPhoneNumber('MX', { message: 'Por favor ingrese un número telefónico válido.' })
+    @Matches(/^[0-9]{10}$/, { message: 'El número telefónico debe contener exactamente 10 dígitos.' })
     @IsNotEmpty()
     phone: string;
 
     @IsNotEmpty()
-    @MinLength(8, { message: 'Password must contain at least 8 chars' })
+    @MinLength(8, { message: 'La contraseña debe contener al menos 8 caractéres.' })
     password: string;
-
 }
